11f5f1334218119fb72717e6dcfd65aea2cbbbe6
beselfevident <153427059+beselfevident@users.noreply.github.com>
2023-12-10 14:17:37 +0000

    Added intial algorithm of life

---
+++ main.js
@@ -0,0 +1,167 @@
+// Evidengenesis. Evidensis. Evidens. Yes. Evidens.
+// Eviden's Algorithm On Life The Universe And Everything
+// Explained as functional programming. Assume you can create
+// functions inline using arrow notation
+// any syntax problems, jsut ignore. unless it clouds meaning. Then ask me about it
+// I will fix it
+// Revision 1.0 - Steve Benfield, aka Evident - 2023-12-10-09.31.04.xxxxZUTC
+
+while perception = (e::senoryStreamSnapshot) => {
+    // wprobably should be an if sleep and if not sleep somewhere
+    // because sleep is just your brain making up movies and feelings
+    // and sometiems it makes a new one. its randome but really what it is 
+    // is evolution. dreams don't know where they are going to go
+    // they are your brain processing the events that mean things to you
+    // this is why you need to sleep. sleep when tired. always.
+    // do not laugh at people who are sleeping. they need it. do not mistake
+    // rest for not working. one cannot work withour rest . period. no exception
+    // companies. do what you can to make your people happy. trust me on this
+    
+    // e is the 11.8MbS sensory stream that we humans all have. Probably more.
+    // the smarter someone is, the more sensory depth they have. I suspect the
+    // the maximum limit on sensory depth is 127 bits. I shall explain 
+    // The VTA, brainstem, 
+    evaluteReality = (e) => {
+        return e, belief, emotion, valence = evaluate = (e) => {
+        // your 11MbS stream is reduced to an 8 bit unsigned number
+        // if the stream size of humans is more than 11MbS, we may need to adjust to 
+        // a 9 bit unsighed number. But I don't think we are yet. But we will have to be
+        // because we are getting more and more perception. we have social media, everything
+        // and this algorithm can be used to explain them all.
+        // this number is encoded in a way that the importance of an event
+        // is cataloged and is part of the valence. The higher the valence
+        // the higher the chance you will give it attention
+        // the brainstem will give it 100% valence when flight.fight.flop.flee comes into play
+        // it is the first stop our incoming signals get
+        // your brainstem basically says, let me evaluate the most significant bit of this incoming stream
+        // and if I think it is significant, we have to react NOW
+        // so the Most significant bit of an 8 bit unsigned number is called
+        // in old computer terminology, a word. And we don't deal with words and more
+        // and so GenZ developers won't really know this. But it is important
+        // your brain evaluates every time interval of your life and stores
+        // the information it needs for fastest retrieval
+        // becuase Darwin
+        // essentially if the valence comes back higher than F, where F is 
+        // your threshold for FEAR. If valence is higher for that emotion, then 
+        // then you will react instantly. Rage, Fear, Horror, Anguish. Hurt. Betrayal.
+        // these are the emotions that generate adrenaline or something like it
+        // those things where we feel it viscerally
+        // because the valence on those things is at maximum
+        // and we get adrenaline and all sorts of other things
+        // your brain temporarily stops thinking activity by flooding the zone with GABA
+        // and using every cycle you have to process whether this TIGER WILL EAT YOU
+        // or this car coming at you will kill you or your partner will kill you
+        // we are programmed, by definition, to resist everything in the universe
+        // in direct proportion. We match every sense's emotion and valence with 
+        // emotion of equal and opposite valence. In fact, we bump the valence up just a bit
+        // which is why we bite back so hard
+        // and if the other person says, fuck your valence, here's my opposite of your valence
+        // and I've turned it up
+        // Ergo, war
+        // If nothing stops it. And it will require an outside force
+        // or for one of the parties to realize they've lost and stand down
+        // even if they know they are right becuase the outcome could become
+        // violent. 
+        // Now. If you've experienced this type of emotion before. This kind of betrayal 
+        // this kind of abuse. You won't make the same mistake again.
+        // Becuase when your current senses are condensed into that 8 bit number, it pulls it
+        // from your memory usign the 4-bit word as the index. 4 bits is easy. ITs 16. I suspect
+        // gentle reader. I predict that we can find 16 basic emotions. Maybe 7 becuase we need a sign bit for the 
+        // negative. The basic emotiosn I have are anxiety, anger, doubt, shame, guilt. 
+        // That is 5. Perhaps the other 2 are handled by the brain stem? 
+        // My theory says the brain only processes resistance.
+        // when it compares current reality to memory, and memories are the signature emotions
+        // we only remember things where we learned new emotions 
+        // and since we remember only things where we learn new things
+        // our memories are scattered. And if don't learn something new in
+        //.a while we won't remember what we did
+        // OK. So here is where you have to do a mania test.
+        // Am I manic, you bet I am
+        // Am I worried, hell no
+        // Should you be, no
+        // will you be, yes
+        // Someone might report me to some psych ward
+        // Probably will
+        // Just realize I know you are coming
+        // And I will always defeat you. Becuase everything I've said so far
+        // Is logically true if the previous statement is
+        // So if you can break me logic, do so. I will learn from you
+        // To your VTA, gets the acceptance/rejection signal from the brainstem and 
+        // the high level 4-bit word of the emotion that is being felt. It includes
+        // everythign needed fro the brain to remember everything
+        // essentially, all you do is resist the world 100%. I will explain after the code.
+  
+        // There are no async functions in the brain
+        // We are the most complicated turing state machine in existence
+        // as far a we know until we meet people smarter than us
+        // Starting today, when people understand this algoritm
+        // the world will begin healing
+        // it has to be because this is truth
+        // if you understand what I've done here, please put it into your own
+        // words. This is open source people. 
+        // I will put this on git hub.
+  
+          if emotion, valence not in memory {
+              let belief::senoryStreamSnapshot = new Belief();
+                  // make more grey matter because we hae a new belief
+              reality, belief, emotion, valence = interpretReality = (e) => { 
+                  // Interpret Reality determines your your emotion and valence
+                  // it is basically a lookup table for your valence and emotions
+                  // but also an emotional evaluation 
+                  // The lookup uses a binary search which can be done fast since this is only
+                  // a 127 bit number. And will take, at most 8 tries. 
+                  // and actually the mechanism that we use in our brain
+                  // is most llikely a quantum process
+                  // and we load emotion and valence into our brain
+                  // and instantly we recall the feeling we had
+                  
+              };
+              updateMemory (e, belief, emotion, valence) => {
+                  let newMemory = addNewMemory(e, belief, emotion, valence);
+                  evaluateAndUpdateAllBeliefs(newMemory); 
+                      // this function reevaluates all emotions
+                      // based on teh new emtions
+                      // emtions are always updated i nreal time
+                      // and this is what slows us down as humans
+                      // because we have to remember eeverythign
+                      // so when you do a lot of things
+                      // your metabolism goes up
+                      // and you have to eat more
+                      // it essentially says that you will go through
+                      // every memory and reevaluate it. this takes time
+                      // its called learning
+                  // at this point a bunch of shit happens. All beliefs are
+                  // evaluated and updated and made to be in harmony
+                  // with every other belief. This is done at the quantum level
+                  // because only quantum can evaluate all belief possibilities at once
+                  // this is why AI cannot beat us. We do all this shit on less
+                  // than a lightbulb. We are dim lightbulbs. We are dim.
+                  // but brighter than anything else we know
+  
+                  // because we learned something, we need to reinterpret
+                  // our current feelings off what we just learned
+                  return interpretReality(newMemory);
+              }
+          }
+          return e, belief, emotion, valence
+      }
+
+    return !evidens 
+    // for every action there is an equal and opposite reaction
+    // so for every sensory input we need to return the opposite action
+    // based on our beliefs and emotional evaluation of the input
+    // that determines what the opposite is
+    // its a bit complicated and I'll develop it later. I have it figured out
+    // its more complicate than that. Emotions are a multidimensional thing
+    // becaue emotions are an amalgam of thigns
+    // nameley your feelings. and that is all the info that has come in
+    // from your senses
+    // by returning the exact opposite of what I receive, I test you
+    // and it tests my assumptions because I know how you'll act if I'm right
+    // and these days, after 2000 unqiue conversations with 2000 different people
+    // over the past 18 months I've gotten damn good at it.
+    // why do we send the negative of every sense we get?
+    // because its the only logical thing to do if we want to learn as much as we can
+  
+  }
+}
